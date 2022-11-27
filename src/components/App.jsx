ee7e29f10870f62a86e922e1f7d2b34956576467
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-11-27 01:42:09 +0100

    Update goit-react-hw-02-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,75 @@
 import { Component } from "react";
-import { FeedBack } from "./Feedback/Feedback";
 
-export class App extends Component{
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
   render() {
     return (
-    <>
-      <FeedBack/>
-    </>
-  );
+      <section className="section">
+        <h2 className="sectionTitle">Please leave feedback</h2>
+        <div className="btnWrapper">
+          <button
+            type="button"
+            className="btnGood"
+            onClick={() =>
+              this.setState(prevState => ({ good: prevState.good + 1 }))
+            }
+          >
+            Good
+          </button>
+          <button
+            type="button"
+            className="btnNeutral"
+            onClick={() =>
+              this.setState(prevState => ({ neutral: prevState.neutral + 1 }))
+            }
+          >
+            Neutral
+          </button>
+          <button
+            type="button"
+            className="btnBad"
+            onClick={() =>
+              this.setState(prevState => ({ bad: prevState.bad + 1 }))
+            }
+          >
+            Bad
+          </button>
+        </div>
+        <h3 className="listTitle">Statistics</h3>
+        <ul className="Statistics">
+          <li className="statisticsItem">
+            Good:<span className="good">{this.state.good}</span>
+          </li>
+          <li className="statisticsItem">
+            Neutral:<span className="neutral">{this.state.neutral}</span>
+          </li>
+          <li className="statisticsItem">
+            Bad:<span className="bad">{this.state.bad}</span>
+          </li>
+          <li className="statisticsItem">
+            Total:<span className="Total">{this.countTotalFeedback()}</span>
+          </li>
+          <li className="statisticsItem">
+            Positive feedback:
+            <span className="positive">
+              {this.countPositiveFeedbackPercentage() || 0}%
+            </span>
+          </li>
+        </ul>
+      </section>
+    );
   }
 }
-
-// export const App = () => {
-//   return (
-//     <>
-      
-//     </>
-//   );
-// };
